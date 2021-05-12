22454c549b7b4f6e5cf3c7955d49b523c9bbbb2c
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-11 19:17:55 -0500

    running into auth error

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map/map.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @ViewChild("map", { static: true })
+  private mapViewEl?: ElementRef;
+
+  private mapView?: MapView;
 
   constructor() { }
 
   ngOnInit(): void {
+    const map = new Map({
+      basemap: "arcgis-topographic" // Basemap layer service
+    });
+    this.mapView = new MapView({
+      map: map,
+      container: this.mapViewEl?.nativeElement
+    });
   }
 
 }
