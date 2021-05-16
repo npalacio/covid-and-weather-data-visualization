9df0af190ae38379a0a9447293115598c1f612f4
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-16 13:27:42 -0500

    cleanup; next step is symbolizing layer better

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map/map.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map/map.component.ts
@@ -20,25 +20,19 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     config.apiKey = 'AAPK731e0b4f7bf541ec9a2e542735af6990fUotVKYCQgH1Jssz-aMZSQ5pWUQuk3E4HBw4Zy9YVMVMhvEReKg1nLvtRyzRroPw';
     config.assetsPath = './assets';
-    const layer = new FeatureLayer({
-      // Notice that the url doesn't end with /2
+    const countyLayer = new FeatureLayer({
       url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties_Generalized/FeatureServer",
       layerId: 0
     });
     const map = new Map({
       basemap: 'arcgis-streets', // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
-      layers: [layer]
+      layers: [countyLayer]
     });
     this.mapView = new MapView({
       map: map,
       center: [-98.5795, 39.8283], // Longitude, latitude
       zoom: 4,
       container: this.mapViewEl?.nativeElement
-    });
-    layer.when(() => {
-      console.log('counties loaded');
-    }, (err: any) => {
-      console.log(err);
     });
   }
 
