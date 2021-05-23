e7babc412a1cbb02189500aed2012abd5e22f081
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-23 10:37:53 -0500

    initial map state to separate file

---
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/state/map-state.initial.ts
@@ -0,0 +1,24 @@
+import { MapState } from '../shared/models/state/map-state.model';
+
+export const initialMapState: MapState = {
+  basemap: 'arcgis-streets',
+  center: [-98.5795, 39.8283], // Longitude, latitude
+  zoom: 4,
+  layerConfigs: [{
+    url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties_Generalized/FeatureServer",
+    layerId: 0,
+    outFields: ['NAME','STATE_NAME'],
+    renderer: <any>{
+      type: "simple",
+      symbol: {
+        type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+        color: [ 51,51, 204, 0.9 ],
+        style: "solid",
+        outline: {  // autocasts as new SimpleLineSymbol()
+          color: "white",
+          width: 1
+        }
+      }
+    }
+  }]
+}
--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/state/map-state.service.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/state/map-state.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ObservableStore } from '@codewithdan/observable-store';
 import { MapState } from '../shared/models/state';
+import { initialMapState } from './map-state.initial';
 
 @Injectable({
   providedIn: 'root'
@@ -8,30 +9,8 @@ import { MapState } from '../shared/models/state';
 export class MapStateService extends ObservableStore<MapState> {
 
   constructor() {
-    const initialState: MapState = {
-      basemap: 'arcgis-streets',
-      center: [-98.5795, 39.8283], // Longitude, latitude
-      zoom: 4,
-      layerConfigs: [{
-        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties_Generalized/FeatureServer",
-        layerId: 0,
-        outFields: ['NAME','STATE_NAME'],
-        renderer: <any>{
-          type: "simple",
-          symbol: {
-            type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-            color: [ 51,51, 204, 0.9 ],
-            style: "solid",
-            outline: {  // autocasts as new SimpleLineSymbol()
-              color: "white",
-              width: 1
-            }
-          }
-        }
-      }]
-    };
     super({});
-    this.setState(initialState, 'INIT_STATE');
+    this.setState(initialMapState, 'INIT_STATE');
   }
 
   get() {
