57778daa3b9ceeb4f22bff82bac507d5635c336c
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-23 12:46:51 -0500

    setting default route to counties

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/mapping-routing.module.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/mapping-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes, } from '@angular/router';
 import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
-  { path: '', component: MapComponent }
+  { path: 'counties', component: MapComponent },
+  { path: '**', redirectTo: 'counties' }
 ];
 
 @NgModule({
