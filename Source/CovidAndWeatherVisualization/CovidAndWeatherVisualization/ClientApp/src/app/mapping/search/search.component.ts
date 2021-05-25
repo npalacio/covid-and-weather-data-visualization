efbdbe2567065e100213413592dc14c8aa2585d1
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-24 19:55:12 -0500

    clearing suggestions when less than 3 chars

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/search/search.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/search/search.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { from, Observable, OperatorFunction } from 'rxjs';
+import { from, Observable, of, OperatorFunction } from 'rxjs';
 import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 import { County } from 'src/app/shared/models/state';
 import { MapService } from '../map.service';
 import { MapStateService } from '../../state/map-state.service';
-import { MapState } from '../../shared/models/state/map-state.model';
 
 @Component({
   selector: 'app-search',
@@ -20,11 +19,14 @@ export class SearchComponent implements OnInit {
   }
 
   search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    // TODO: Limit # of suggested results
     return text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      filter(term => term.length > 2),
       switchMap(term => {
+        if(term.length < 3) {
+          return of([]);
+        }
         return from(this.mapService.queryCounties(term)).pipe(
           map(() => {
             return this.mapStateService.getCountySearchResults().map((county: County) => {
