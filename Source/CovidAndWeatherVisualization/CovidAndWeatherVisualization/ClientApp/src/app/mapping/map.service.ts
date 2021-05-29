7e6866e630fa5bb8911fe3f653dccc489aee6cb4
Nick Palacio <nick.palacio@fcsamerica.com>
2021-05-29 09:46:23 -0500

    map service setup to use FID not OBJECTID to identify features

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map.service.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/mapping/map.service.ts
@@ -13,13 +13,23 @@ import { MapState } from '../shared/models/state';
 export class MapService {
   private mapView?: MapView;
   private countyLayer: FeatureLayer = {} as any;
+  private countyLayerView?: FeatureLayerView;
+  private highlightedCounty?: __esri.Handle;
 
   constructor(private mapStateService: MapStateService) {
-    this.mapStateService.stateChanged.subscribe((mapState: MapState) => {
+    this.mapStateService.stateChanged.subscribe(async (mapState: MapState) => {
       if(mapState.selectedCounty) {
-
         // In the end we want to be zooming somewhere here
         // We only want to zoom when they select a county
+        var countyGraphic = await this.getCountyGraphic(mapState.selectedCounty.objectId);
+        if(this.highlightedCounty) {
+          this.highlightedCounty.remove();
+        }
+        this.highlightedCounty = this.countyLayerView?.highlight(mapState.selectedCounty.objectId);
+        this.mapView?.goTo({
+          target: countyGraphic,
+          zoom: 8
+        });
       }
     });
    }
@@ -43,23 +53,22 @@ export class MapService {
       container: mapHtmlElement?.nativeElement
     });
     this.mapView.whenLayerView(this.countyLayer).then((layerView: FeatureLayerView) => {
-      console.log('loaded feature layer view');
-      console.log(layerView);
+      this.countyLayerView = layerView;
     });
   }
 
   async queryCounties(searchTerm: string, recordCount: number): Promise<void> {
     const query = {
       where: `STATE_NAME LIKE \'${searchTerm}%\' OR NAME LIKE\'${searchTerm}%\'`,
-      returnGeometry: false,
-      outFields: ['OBJECTID', 'NAME', 'STATE_NAME'],
+      returnGeometry: true,
+      outFields: ['FID', 'NAME', 'STATE_NAME'],
       num: recordCount
     };
 
     const counties: County[] = await this.countyLayer?.queryFeatures(query).then(result => {
-      return result.features.map(feature => {
+      return result.features.map((feature): County => {
         return {
-          objectId: feature.attributes.OBJECTID,
+          objectId: feature.attributes.FID,
           name: feature.attributes.NAME,
           state: feature.attributes.STATE_NAME
         };
@@ -68,6 +77,18 @@ export class MapService {
 
     // Push new search results onto state
     this.mapStateService.setCountySearchResults(counties);
+  }
+
+  private async getCountyGraphic(objectId: number): Promise<__esri.Graphic> {
+    const query = {
+      where: `FID = ${objectId}`,
+      returnGeometry: true,
+      outFields: ['FID']
+    };
+
+    return await this.countyLayer?.queryFeatures(query).then(result => {
+      return result.features[0]
+    });
   }
 
 }
