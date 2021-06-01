72a5d88b5aa95522ef5331548ae5bc5fa20eb92a
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-01 16:57:28 -0500

    renaming function

--- Source/CovidDataLoad/CovidDataLoad/DataLoad.cs
+++ Source/CovidDataLoad/CovidDataLoad/DataLoad.cs
@@ -5,10 +5,10 @@ using Microsoft.Extensions.Logging;
 
 namespace CovidDataLoad
 {
-    public static class Function1
+    public static class DataLoad
     {
-        [FunctionName("Function1")]
-        public static void Run([TimerTrigger("*/5 * * * *")]TimerInfo myTimer, ILogger log)
+        [FunctionName("DataLoad")]
+        public static void Run([TimerTrigger("*/15 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
