c28d84460c1ff6828ebb7e95e685a11d3cafbb68
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-02 18:59:34 -0500

    working version reading covid data to list of models

--- Source/CovidDataLoad/CovidDataLoad/DataLoad.cs
+++ Source/CovidDataLoad/CovidDataLoad/DataLoad.cs
@@ -1,16 +1,32 @@
 using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using CovidDataLoad.Models;
+using CsvHelper;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CovidDataLoad
 {
     public static class DataLoad
     {
         [FunctionName("DataLoad")]
-        public static void Run([TimerTrigger("0 7 * * *")]TimerInfo myTimer, ILogger log)
+        public static async void Run([TimerTrigger("* * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv");
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            using (var streamReader = new StreamReader(stream))
+            using (var csv = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<CovidCumulativeByCounty>();
+                log.LogInformation($"Record Count: {records.Count()}");
+            }
         }
     }
 }
---
+++ Source/CovidDataLoad/CovidDataLoad/Models/CovidCumulativeByCounty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace CovidDataLoad.Models
+{
+    public class CovidCumulativeByCounty
+    {
+        [Name("date")]
+        public DateTime Date { get; set; }
+        [Name("county")]
+        public string County { get; set; }
+        [Name("state")]
+        public string State { get; set; }
+        [Name("fips")]
+        public int? Fips { get; set; }
+        [Name("cases")]
+        public int Cases { get; set; }
+        [Name("deaths")]
+        public int? Deaths { get; set; }
+    }
+}
