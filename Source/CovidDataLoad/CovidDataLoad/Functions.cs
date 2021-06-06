0c548551811a4e7f57a4f6c22c8f9239e7034ec7
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-06 11:25:02 -0500

    testing out read from DB in azure

--- Source/CovidDataLoad/CovidDataLoad/Functions.cs
+++ Source/CovidDataLoad/CovidDataLoad/Functions.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using CovidDataLoad.Interfaces;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace CovidDataLoad
@@ -16,11 +19,34 @@ namespace CovidDataLoad
         }
 
         [FunctionName("DataLoad")]
-        public async void Run([TimerTrigger("0 12 * * *")] TimerInfo myTimer, ILogger log)
+        //public async void Run([TimerTrigger("0 12 * * *")] TimerInfo myTimer, ILogger log)
+        public async void Run([TimerTrigger("* */5 * * *")] TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"DataLoad function started at {DateTime.Now}");
-            var covidData = await _covidRepo.GetCovidCumulativeDataByCounty();
-            log.LogInformation($"Record count: {covidData.Count()}");
+            //log.LogInformation($"DataLoad function started at {DateTime.Now}");
+            //var covidData = await _covidRepo.GetCovidCumulativeDataByCounty();
+            //log.LogInformation($"Record count: {covidData.Count()}");
+            await ReadFromDb(log);
+        }
+
+        private async Task ReadFromDb(ILogger log)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("connection-string-db-capstone");
+            log.LogInformation($"Conn string: {connectionString}");
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var text = "SELECT TOP (1000) [AddressID],[AddressLine1],[AddressLine2],[City],[StateProvince],[CountryRegion],[PostalCode],[rowguid],[ModifiedDate] FROM [SalesLT].[Address]";
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var dataReader = await cmd.ExecuteReaderAsync();
+                    DataTable dt = new DataTable();
+                    dt.Load(dataReader);
+                    int numRows = dt.Rows.Count;
+                    log.LogInformation($"{numRows} rows were read");
+                }
+            }
         }
     }
 }
