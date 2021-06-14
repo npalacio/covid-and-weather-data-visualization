22376cbc8d2e05f48e350cbd8343252d718f3184
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-14 08:30:59 -0500

    path filter

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/Startup.cs
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/Startup.cs
@@ -40,7 +40,6 @@ namespace CovidAndWeatherVisualization
                 o.EnableRetryOnFailure();
             }));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            //trigger build
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
