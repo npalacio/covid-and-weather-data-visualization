d97d13c958230f9d9e28c909378d245b37a955bc
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-18 17:18:42 -0500

    iterating

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/charts/charts.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/charts/charts.component.ts
@@ -9,19 +9,18 @@ import { Color, Label } from 'ng2-charts';
 })
 export class ChartsComponent implements OnInit {
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', fill: false },
   ];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: ChartOptions = {
-    responsive: true,
+    responsive: true
   };
   public lineChartColors: Color[] = [
     {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'black'
     },
   ];
-  public lineChartLegend = true;
+  public lineChartLegend = false;
   public lineChartType: ChartType = 'line';
   public lineChartPlugins = [];
 
