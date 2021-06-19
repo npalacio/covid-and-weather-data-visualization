83aa8b349b0e69e1636ab786da36998de6e2bd4b
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-19 12:20:21 -0500

    extracting chart config to separate file

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-covid/chart-covid.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-covid/chart-covid.component.ts
@@ -4,6 +4,7 @@ import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 import { CovidStateService } from 'src/app/state';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { chartConfigs } from '../charts-config';
 
 @Component({
   selector: 'app-chart-covid',
@@ -11,80 +12,30 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./chart-covid.component.scss']
 })
 export class ChartCovidComponent implements OnInit {
-  lineChartData: ChartDataSets[] = [
-    { data: [], fill: false, pointRadius: 0, pointHitRadius: 2 }
-  ];
-  lineChartLabels: Label[] = [];
-  lineChartOptions: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    title: {
-      display: true,
-      text: 'COVID Infections'
-    },
-    scales: {
-      yAxes: [{
-        display: true,
-        ticks: {
-          suggestedMin: 0,
-          callback: (value, index, values) => this.tickFormatter(+value)
-        },
-        scaleLabel: {
-          display: true,
-          labelString: 'Confirmed Infections'
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Date'
-        }
-      }]
-    }
-  };
-  lineChartColors: Color[] = [
+  labels: Label[] = [];
+  colors: Color[] = [
     {
       borderColor: 'black'
     },
   ];
-  lineChartLegend = false;
-  lineChartType: ChartType = 'line';
+  legend = false;
+  type: ChartType = 'line';
   isLoading = false;
+  chartConfig: any;
 
   constructor(private datePipe: DatePipe, private covidStateService: CovidStateService, private spinner: NgxSpinnerService) { }
 
   async ngOnInit(): Promise<void> {
+    this.chartConfig = chartConfigs.covid;
     this.covidStateService.stateChanged.subscribe(state => {
       this.isLoading = state.isLoading;
       if (this.isLoading) {
         this.spinner.show();
       } else {
-        this.lineChartData[0].data = state.cases;
-        this.lineChartLabels = state.dates.map(date => this.datePipe.transform(date, 'MM/dd') ?? '');
+        this.chartConfig.data[0].data = state.cases;
+        this.labels = state.dates.map(date => this.datePipe.transform(date, 'MM/dd') ?? '');
         this.spinner.hide();
       }
     });
-  }
-
-  private tickFormatter(number: number) {
-    if (number == 0) {
-      return 0;
-    }
-    else {
-      // hundreds
-      if (number <= 999) {
-        return number;
-      }
-      // thousands
-      else if (number >= 1000 && number <= 999999) {
-        return (number / 1000) + 'K';
-      }
-      // millions
-      else if (number >= 1000000 && number <= 999999999) {
-        return (number / 1000000) + 'M';
-      }
-      else
-        return number;
-    }
   }
 }
---
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/charts-config.ts
@@ -0,0 +1,56 @@
+export const chartConfigs = {
+  covid: {
+    data: [
+      { data: [], fill: false, pointRadius: 0, pointHitRadius: 5 }
+    ],
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: 'COVID Infections'
+      },
+      scales: {
+        yAxes: [{
+          display: true,
+          ticks: {
+            suggestedMin: 0,
+            callback: (value: string | number, index: any, values: any) => tickFormatter(+value)
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Confirmed Infections'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }]
+      }
+    }
+  }
+}
+
+function tickFormatter(number: number) {
+  if (number == 0) {
+    return 0;
+  }
+  else {
+    // hundreds
+    if (number <= 999) {
+      return number;
+    }
+    // thousands
+    else if (number >= 1000 && number <= 999999) {
+      return (number / 1000) + 'K';
+    }
+    // millions
+    else if (number >= 1000000 && number <= 999999999) {
+      return (number / 1000000) + 'M';
+    }
+    else
+      return number;
+  }
+}
