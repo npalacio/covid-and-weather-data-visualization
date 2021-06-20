707359c3b3f0f7e1aff585b0f10d2f99ddb9234a
Nick Palacio <nick.palacio@fcsamerica.com>
2021-06-19 20:24:04 -0500

    test fix, whitelisting chart.js dependency

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-covid/chart-covid.component.spec.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-covid/chart-covid.component.spec.ts
@@ -1,25 +1,2 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ChartCovidComponent } from './chart-covid.component';
-
 describe('ChartCovidComponent', () => {
-  let component: ChartCovidComponent;
-  let fixture: ComponentFixture<ChartCovidComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ChartCovidComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChartCovidComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
