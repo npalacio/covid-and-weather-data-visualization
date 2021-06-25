74a34da4f920d6f075565b726e94228bdd741396
Nick Palacio <npalacio2240@gmail.com>
2021-06-25 10:35:56 -0500

    dynamically building radio button list

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-settings/chart-settings.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-settings/chart-settings.component.ts
@@ -1,3 +1,4 @@
+import { keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { WeatherChart } from 'src/app/shared/models';
@@ -9,22 +10,19 @@ import { ChartSettingsStateService } from 'src/app/state';
   styleUrls: ['./chart-settings.component.scss']
 })
 export class ChartSettingsComponent implements OnInit {
-  weatherChartTypes: any[] = [
-    {
-      name: 'Temperature',
-      value: WeatherChart.Temperature
-    },
-    {
-      name: 'Humidity',
-      value: WeatherChart.Humidity
-    }
-  ];
+  weatherChartTypes: any[] = [];
 
   radioSelected: WeatherChart = WeatherChart.Temperature;
 
   constructor(private activeModal: NgbActiveModal, private chartSettingsStateService: ChartSettingsStateService) { }
 
   ngOnInit(): void {
+    this.weatherChartTypes = Object.keys(WeatherChart).map((key: any) => WeatherChart[key]).filter(value => typeof value === 'number').map(key => {
+      return {
+        name: WeatherChart[+key],
+        value: +key
+      };
+    });
   }
 
   update(): void {
