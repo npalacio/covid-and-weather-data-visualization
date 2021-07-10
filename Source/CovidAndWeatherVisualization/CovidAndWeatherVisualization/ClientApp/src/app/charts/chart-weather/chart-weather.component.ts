b96284ed813e1658c3fa0db1d5eaa60067ab11c3
Nick Palacio <npalacio2240@gmail.com>
2021-07-10 14:04:42 -0500

    refactor observable stuff

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-weather/chart-weather.component.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/chart-weather/chart-weather.component.ts
@@ -5,6 +5,7 @@ import { WeatherStateService } from '../../state/weather/weather-state.service';
 import { chartConfigs } from '../charts-config';
 import { ChartSettingsStateService } from '../../state/chart/chart-settings-state.service';
 import { WeatherChart } from 'src/app/shared/models';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-chart-weather',
@@ -15,41 +16,32 @@ export class ChartWeatherComponent implements OnInit {
   labels: Label[] = [];
   isLoading = false;
   chartConfig: any;
-  weatherChart?: WeatherChart;
 
   constructor(private datePipe: DatePipe
-            , private weatherStateService: WeatherStateService
-            , private chartSettingsStateService: ChartSettingsStateService) { }
+    , private weatherStateService: WeatherStateService
+    , private chartSettingsStateService: ChartSettingsStateService) { }
 
   async ngOnInit(): Promise<void> {
     this.chartConfig = { ...chartConfigs.temperature };
-    this.chartSettingsStateService.stateChanged.subscribe(state => {
-      this.weatherChart = state.weatherChart;
-      this.updateChartData();
-    });
-    this.weatherStateService.stateChanged.subscribe(state => {
-      this.isLoading = state.isLoading;
-      this.labels = state.dates.map(date => this.datePipe.transform(date, 'MM/dd') ?? 'unknown');
-      this.updateChartData();
-    });
-  }
-
-  private updateChartData(): void {
-    if (!this.isLoading && this.weatherChart) {
-      switch (this.weatherChart) {
-        case WeatherChart.Temperature:
-          this.chartConfig = { ...chartConfigs.temperature };
-          this.chartConfig.data.data = this.weatherStateService.getTemperaturesAverage();
-          break;
-        case WeatherChart.HumidityRelative:
-          this.chartConfig = { ...chartConfigs.humidityRelative };
-          this.chartConfig.data.data = this.weatherStateService.getHumiditiesRelativeAverage();
-          break;
-        case WeatherChart.HumiditySpecific:
-          this.chartConfig = { ...chartConfigs.humiditySpecific };
-          this.chartConfig.data.data = this.weatherStateService.getHumiditiesSpecificAverage();
-          break;
+    combineLatest([this.chartSettingsStateService.stateChanged, this.weatherStateService.stateChanged]).subscribe(([chartSettingsState, weatherState]) => {
+      this.isLoading = weatherState.isLoading;
+      this.labels = weatherState.dates.map(date => this.datePipe.transform(date, 'MM/dd') ?? 'unknown');
+      if (!this.isLoading && chartSettingsState.weatherChart) {
+        switch (chartSettingsState.weatherChart) {
+          case WeatherChart.Temperature:
+            this.chartConfig = { ...chartConfigs.temperature };
+            this.chartConfig.data.data = this.weatherStateService.getTemperaturesAverage();
+            break;
+          case WeatherChart.HumidityRelative:
+            this.chartConfig = { ...chartConfigs.humidityRelative };
+            this.chartConfig.data.data = this.weatherStateService.getHumiditiesRelativeAverage();
+            break;
+          case WeatherChart.HumiditySpecific:
+            this.chartConfig = { ...chartConfigs.humiditySpecific };
+            this.chartConfig.data.data = this.weatherStateService.getHumiditiesSpecificAverage();
+            break;
+        }
       }
-    }
+    });
   }
 }
