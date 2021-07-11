940aec3d881f2ce9248226a80ebea492bb2a282a
Nick Palacio <npalacio2240@gmail.com>
2021-07-11 12:05:35 -0500

    uneeded code

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/state/services/aggregation.service.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/state/services/aggregation.service.ts
@@ -12,11 +12,9 @@ export class AggregationService {
   getWeeklyAverages(data: SelectedData[], startDate: Date): SelectedData[] {
     startDate = new Date(Date.UTC(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDay()));
     const weeklyAverages: SelectedData[] = [];
-    const startDayIndex = data.findIndex(_ => new Date(_.date).getTime() === startDate?.getTime());
-    const numberOfWeeks = Math.floor((data.length - startDayIndex)/7);
-    console.log('start day index = ' + startDayIndex);
+    const numberOfWeeks = Math.floor(data.length/7);
     for (let currentWeek = 0; currentWeek < numberOfWeeks; currentWeek++) {
-      const startIndex = startDayIndex + currentWeek * 7;
+      const startIndex = currentWeek * 7;
       const endIndex = startIndex + 6;
       const currentWeekValues = data.slice(startIndex, endIndex + 1).filter(d => d.value).map(d => d.value);
       weeklyAverages.push({
