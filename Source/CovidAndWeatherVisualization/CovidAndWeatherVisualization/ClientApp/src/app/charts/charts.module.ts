887b5a1b709d0fe263a9d3c97c9e52353b9a4e33
Nick Palacio <npalacio2240@gmail.com>
2021-07-19 14:44:20 -0500

    created share button

--- Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/charts.module.ts
+++ Source/CovidAndWeatherVisualization/CovidAndWeatherVisualization/ClientApp/src/app/charts/charts.module.ts
@@ -10,7 +10,7 @@ import { ChartCovidComponent } from './chart-covid/chart-covid.component';
 import { SharedModule } from '../shared/shared.module';
 import { ChartWeatherComponent } from './chart-weather/chart-weather.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { NgbDatepickerModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerModule, NgbModalModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChartSettingsComponent } from './chart-settings/chart-settings.component';
 import { FormsModule } from '@angular/forms';
 import { ChartScatterComponent } from './chart-scatter/chart-scatter.component';
@@ -34,6 +34,7 @@ import { ChartScatterComponent } from './chart-scatter/chart-scatter.component';
     NgxSpinnerModule,
     NgbModalModule,
     NgbDatepickerModule,
+    NgbTooltipModule,
     SharedModule
   ],
   exports: [
